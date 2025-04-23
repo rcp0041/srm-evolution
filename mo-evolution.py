f75785c0ddf7cb6e91e74f1b15d7e9155b6dd547
Ray Patrick <rcp0041@auburn.edu>
2025-04-23 15:36:55 -0500

    Removed problems to their own module

--- mo-evolution.py
+++ mo-evolution.py
@@ -6,11 +6,9 @@ GENOME:
     [N, Ro, Ri, Rp, f, epsilon, theta/2, grain_length, prop_i, material_i]
 """
 
-from numpy import array
-from tools import construct_motor, is_physical, init_bounds, print_genome
-from tools import get_setting
+from tools import get_setting, init_bounds, construct_motor, print_genome
+from problems import MaxPayload_MinPressure
 
-from leap_ec.multiobjective.problems import MultiObjectiveProblem   
 from leap_ec.representation import Representation
 from leap_ec.ops import clone, evaluate, pool
 from leap_ec.ops import random_selection
@@ -30,29 +28,45 @@ elif selector_type == 'tournament':
 pop_size = get_setting('pop_size')
 max_generations = get_setting('generations')
 
-class MO_Rocket_Problem(MultiObjectiveProblem):
-    def evaluate(self,phenome):
-        motor = construct_motor(phenome)
-        if is_physical(motor) == False:
-            return array([0,0])
-        else:
-            return array([motor.payload_mass(),1/motor.max_pressure()])
+from tools import is_physical_genome
+def initialize_physical_motor(bounds):
+    """ Keeps trying until it gets a physically-valid motor """
+    def create():
+        create_genome = create_real_vector(bounds=bounds)
+        physicality = False
+        while physicality == False:
+            genome = create_genome()
+            physicality = is_physical_genome(genome)
+        return genome
+    return create
+    
+def evolve_multiple_parameters(problem):
+    return generalized_nsga_2(
+        max_generations=max_generations, pop_size=pop_size,
+        problem=problem(problem.maximize),
+        representation=Representation(
+            # initialize=create_real_vector(bounds=bounds)
+            initialize=initialize_physical_motor(bounds=bounds)
+        ),
+        pipeline=[
+            selector,
+            clone,
+            mutate_gaussian(std=std,
+                            expected_num_mutations=expected_num_mutations,
+                            bounds=bounds),
+            evaluate,
+            pool(size=pop_size),
+        ]
+    )
 
-final_pop = generalized_nsga_2(
-    max_generations=max_generations, pop_size=pop_size,
-    problem=MO_Rocket_Problem([1,1]),
-    representation=Representation(
-        initialize=create_real_vector(bounds=bounds)
-    ),
-    pipeline=[
-        selector,
-        clone,
-        mutate_gaussian(std=std,
-                        expected_num_mutations=expected_num_mutations,
-                        bounds=bounds),
-        evaluate,
-        pool(size=pop_size),
-    ]
-)
+final_pop = evolve_multiple_parameters(MaxPayload_MinPressure)
 
+def fitness_list():
+    for individual in final_pop:
+        print(individual.fitness)
+
+motor = construct_motor(final_pop[0].genome)
+print(f"Max pressure: {round(motor.max_pressure(),3)} Payload mass: {round(motor.payload_mass(),3)} Average thrust: {round(motor.avg_thrust(),3)} Max thrust: {round(motor.max_thrust(),3)}")
+if motor.grain.halfTheta < 0:
+    print(f"theta/2 = {motor.grain.halfTheta} ... wtf")
 print_genome(final_pop[0].genome)
